44cec7e449dd08fc7274ec866f3474b6632b8398
Nattee Niparnan <nattee@gmail.com>
2014-09-15 18:23:05 +0700

    fix barcode again

--- exercise/ex02/m2_barcode/solution/main.cpp
+++ exercise/ex02/m2_barcode/solution/main.cpp
@@ -1,82 +1,32 @@
 #include <iostream>
-#include <string.h>
 
 using namespace std;
 
-void ResizeArray(int** ary, int* cap, int length)
-{
-	if (length > *cap)
-	{
-		int newCap = length + length / 2;
-		int* tmp = new int[newCap];
-		if (*ary)
-		{
-			memcpy(tmp, *ary, sizeof(int) * (*cap));
-			delete[] (*ary);
-		}
-		*ary = tmp;
-		*cap = newCap;
-	}
+int table[32][32];
+
+int barcode_topdown(int n,int k,int m) {
+  if (n <= 0) return 0;
+  if (k == 1) {
+    if (n <= m) return 1;
+    if (n > m) return 0;
+  }
+  if (table[n][k] > -1) return table[n][k];
+  int sum = 0;
+  for (int i = 1;i <= m;i++) {
+    sum+=barcode_topdown(n-i,k-1,m);
+  }
+  table[n][k] = sum;
+  return sum;
 }
 
-void DeleteArray(int* ary, int length, int offset, int dLen)
-{
-	if (offset >= length - dLen) return;
-	dLen = min(dLen, length - (offset + dLen));
-	memcpy(ary + offset, ary + (length - dLen), sizeof(int) * dLen);
-}
 
-int BarcodeCounting(int N, int M, int K)
-{
-	int* B = 0;
-	int cap = 0;
-	int len = 1;
+int main() {
+  int n,m,k;
+  cin >> n >> m >> k;
+  k++;
+  for (int i = 0;i <= n;i++)
+    for (int j = 0;j <= k;j++)
+      table[i][j] = -1;
+  cout << barcode_topdown(n,k,m) << endl;
 
-	K++;
-	ResizeArray(&B, &cap, K * len);
-	for (int i = 0; i < K; ++i) B[i] = 1;
-
-	for (int i = K; i < N; ++i)
-	{
-		int tLen = len;
-		for (int j = 0; j < len; ++j)
-		{
-			int* b = B + j * K;
-			for (int k = K - 1; k >= 0; --k)
-			{
-				if (b[k] < M)
-				{
-					ResizeArray(&B, &cap, K * (++tLen));
-					b = B + j * K;
-					int* tb = B + (tLen - 1) * K;
-					memcpy(tb, b, sizeof(int) * K);
-					tb[k]++;
-				}
-				if (b[k] != 1) break;
-			}
-		}
-		DeleteArray(B, tLen * K, 0, len * K);
-		len = tLen - len;
-	}
-	
-	// print debug
-	//for (int j = 0; j < len; ++j)
-	//{
-	//	int* b = B + j * K;
-	//	int i = 0;
-	//	for (int k = 0; k < K; ++k)
-	//		for (int l = 0; l < b[k]; ++l)
-	//			printf("%d", k % 2);
-	//	printf("%\n");
-	//}
-
-	delete[] B;
-	return len;
-}
-
-int main(int argc, char *argv[])
-{
-    int N, M, K;
-    scanf("%d %d %d", &N, &M, &K);
-	printf("%d\n", BarcodeCounting(N, M, K));
 }
