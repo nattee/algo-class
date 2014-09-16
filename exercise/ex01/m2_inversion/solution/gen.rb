b5ab78c107933450def2173eebf4f362ba9a4e2a
Nattee Niparnan <nattee@gmail.com>
2014-09-16 18:54:10 +0700

    fix typo and bug in generator of ex00/e2 and ex01/m2

--- exercise/ex01/m2_inversion/solution/gen.rb
+++ exercise/ex01/m2_inversion/solution/gen.rb
@@ -6,7 +6,6 @@ def genOneFile( filename, n, min = -100_000, max = 100_000)
   end
 end
 
-
 genOneFile('../data/1.in',10,0,100)
 genOneFile('../data/2.in',100,-100,100)
 genOneFile('../data/3.in',1000,-10,10)
@@ -16,5 +15,5 @@ genOneFile('../data/5.in',50000)
 File.open('../data/6.in',"w") do |file|
   a = (0..49999).to_a.reverse
   file.puts 50000
-  file.puts a
+  file.puts a.join(" ")
 end
