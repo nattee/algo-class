a5891ed5be1cb82ea7766e37e69459bfed02b2c8
groupdoll <GroupDoll>
2014-11-04 23:27:52 +0700

    improve ex04/e3_teamwork python solution

--- exercise/ex04/e3_teamwork/solution/main.py
+++ exercise/ex04/e3_teamwork/solution/main.py
@@ -1,11 +1,10 @@
-from heapq import *
 N, M = map(int, raw_input().split())
 time = map(int, raw_input().split())
 time.sort()
 workertime = [0]*N
 ans = 0.
-for t in time:
-	mintime = heappop(workertime)
+for i,t in enumerate(time):
+	mintime = workertime[i]
 	ans += mintime+t
-	heappush(workertime, mintime+t)
+	workertime += [mintime+t]
 print ans/M
